2d71b7e372884abd2d96650e3e9a30b3f84c273c
ganijon <tgani96@gmail.com>
2019-10-23 02:21:09 +0300

    Navigation Edited, Html errors removed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Swiper Slider of Portfolio
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
@@ -16,8 +17,9 @@ var swiper = new Swiper('.swiper-container', {
     },
 });
 
-window.sr = ScrollReveal();
 
+// ScrollReveal Plugin
+window.sr = ScrollReveal();
 sr.reveal('nav', {
     duration: 2000,
     origin: 'bottom'
@@ -25,13 +27,13 @@ sr.reveal('nav', {
 sr.reveal('.name', {
     duration: 2000,
     origin: 'top',
-    distance: '300px'
+    distance: '50px'
 });
 
 sr.reveal('.basic-info', {
     duration: 2000,
     origin: 'right',
-    distance: '100px'
+    distance: '40px'
 });
 sr.reveal('#introduction', {
     duration: 2000,
@@ -54,31 +56,24 @@ sr.reveal('.experince-title', {
 sr.reveal('.job-title', {
     duration: 1000,
     origin: 'bottom',
-    distance: '50px',
+    distance: '20px',
     viewFactor: 0.2,
     delay: 500
 });
 sr.reveal('.job-info', {
-    duration: 1500,
+    duration: 1000,
     origin: 'bottom',
-    distance: '50px',
+    distance: '20px',
     viewFactor: 0.2,
-    delay: 500
+    delay: 1000
 });
 sr.reveal('.experince-title', {
-    duration: 1500,
+    duration: 1000,
     origin: 'bottom',
     distance: '50px',
     viewFactor: 0.1,
 
 });
-sr.reveal('.experince-title', {
-    duration: 1500,
-    origin: 'bottom',
-    distance: '50px',
-    viewFactor: 0.1
-});
-
 sr.reveal('.portfolio-box', {
     duration: 1500,
     origin: 'left',
@@ -101,20 +96,20 @@ sr.reveal('.portfolio-title ', {
 sr.reveal('.frameworks-frame', {
     duration: 600,
     origin: 'left',
-    distance: '100px',
+    distance: '50px',
     viewFactor: 0.1
 });
 sr.reveal('.web-frame', {
     duration: 600,
     origin: 'left',
-    distance: '100px',
+    distance: '50px',
     viewFactor: 0.1,
     delay: 600
 });
 sr.reveal('.programming-frame', {
     duration: 600,
     origin: 'left',
-    distance: '100px',
+    distance: '50px',
     viewFactor: 0.1,
     delay: 1200
 });
@@ -137,14 +132,22 @@ $(function () {
     });
 });
 
-
+//Adding and Removing of Active Class for Mobile on Navigation
 $(document).ready(function () {
     $('.menu-toggle').click(function () {
         $('.menu-toggle').toggleClass('active')
         $('nav').toggleClass('active')
+    });
+    $('.nav-btn').click(function (){
+        $('.menu-toggle').removeClass('active')
+        $('nav').removeClass('active')
     })
-})
+});
 
+//Scroll Up btn appear after 300px scrolling
+$(document).ready(function (){
+    $('.arrow-up').hide()
+    });
 $(window).scroll(function(){
   if($(window).scrollTop() > 300){
       $(".arrow-up").fadeIn("slow");
