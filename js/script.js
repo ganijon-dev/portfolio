b266f419d581f01dc0cc4aa16a98b56e59c8758c
ganijon <tgani96@gmail.com>
2019-11-22 23:06:44 +0300

    new resume and arrow added

--- js/script.js
+++ js/script.js
@@ -14,7 +14,12 @@ var swiper = new Swiper('.swiper-container', {
     },
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 });
 
 
